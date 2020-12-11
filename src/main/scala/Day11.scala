59ee5d4c61528a1af449567e03ab9183963136ac
Edvin Lundberg <edvin.lundberg@bynk.se>
2020-12-11 19:29:21 +0100

    Day11 part2 partial.

--- src/main/scala/Day11.scala
+++ src/main/scala/Day11.scala
@@ -25,17 +25,39 @@ object Day11 extends Day[Long, Long] {
     if (next == initial) next else iterateUntilStabilized(next)
   }
 
+  def prettyPrintMap(m: Map[Pos, PosState]): String = {
+    val mC = m.map { case (p, state) => state match {
+      case Occupied => p -> '#'
+      case Empty => p -> 'L'
+    }}
+    (for {
+      y <- 0 to m.keys.map(_.y).max
+      line = (for {
+        x <- 0 to m.keys.map(_.x).max
+      } yield mC.getOrElse(Pos(x, y), '.')).mkString
+    } yield line).mkString("\n")
+  }
+
   def iterateUntilStabilized2(initial: Map[Pos, PosState]): Map[Pos, PosState] = {
+    println(prettyPrintMap(initial))
     val next = (for {
       (p, state) <- initial
-      adjacentOcc = Dir.all8Dirs.map(p + _).flatMap(pos => initial.get(pos)).count(_ == Occupied)
+      firstInAllDirs: Map[Double, Pos] = initial.keySet.excl(p).map(p2 => (p2, PolarCoordinate.make(p, p2))).groupBy(_._2.theta).map {
+        case (theta, posPolar) => (theta, posPolar.toList.sortBy(_._2.r).map(_._1).head)
+      }
+      _ = println(s"For $p")
+      _ = println(firstInAllDirs)
+      seenOccupied = Dir.all8Dirs.flatMap { dir =>
+        val theta = PolarCoordinate.make(p, p + dir).theta
+        firstInAllDirs.get(theta)
+      }.count(_ == Occupied)
       newState = state match {
-        case Occupied if adjacentOcc >= 4 => state.flip
-        case Empty if adjacentOcc == 0 => state.flip
+        case Occupied if seenOccupied >= 5 => state.flip
+        case Empty if seenOccupied == 0 => state.flip
         case _ => state
       }
     } yield p -> newState).toMap
-    if (next == initial) next else iterateUntilStabilized(next)
+    if (next == initial) next else iterateUntilStabilized2(next)
   }
 
   def part1(in: String) = Task.effect {
@@ -53,7 +75,17 @@ object Day11 extends Day[Long, Long] {
   }
 
   def part2(in: String) = Task.effect {
-    ???
+    val lines = in.split("\n")
+    val m: Map[Pos, PosState] = (for {
+      r <- 0 until lines.size
+      c <- 0 until lines(r).size if lines(r)(c) != '.'
+      state = lines(r)(c) match {
+        case 'L' => Empty
+        case '#' => Occupied
+      }
+    } yield Pos(c, r) -> state).toMap
+
+    iterateUntilStabilized2(m).map(_._2).count(_ == Occupied)
   }
 
   val inputs = Map(
--- src/main/scala/Util.scala
+++ src/main/scala/Util.scala
@@ -1,62 +1,74 @@
 object Util {
-	def lowerBound[V <% Ordered[V]](first: BigInt, last: BigInt, value: V, valueOf: BigInt => V): BigInt = {
-		// Adjusted from https://en.cppreference.com/w/cpp/algorithm/lower_bound
-		var f = first
-		var count: BigInt = last - first
+  def lowerBound[V <% Ordered[V]](first: BigInt, last: BigInt, value: V, valueOf: BigInt => V): BigInt = {
+    // Adjusted from https://en.cppreference.com/w/cpp/algorithm/lower_bound
+    var f = first
+    var count: BigInt = last - first
 
-		while (count > 0) {
-			val step = count / 2
-			val i = f + step
-			if (valueOf(i) < value) {
-				f = i + 1
-				count -= step + 1
-			} else count = step
-		}
+    while (count > 0) {
+      val step = count / 2
+      val i = f + step
+      if (valueOf(i) < value) {
+        f = i + 1
+        count -= step + 1
+      } else count = step
+    }
 
-		f
-	}
+    f
+  }
 
-	def time[R](block: => R): R = {
-    	val t0 = System.currentTimeMillis()
-    	val result = block    // call-by-name
-    	val t1 = System.currentTimeMillis()
-    	println("Elapsed time: " + (t1 - t0) + "ms")
-    	result
-	}
+  def time[R](block: => R): R = {
+    val t0 = System.currentTimeMillis()
+    val result = block // call-by-name
+    val t1 = System.currentTimeMillis()
+    println("Elapsed time: " + (t1 - t0) + "ms")
+    result
+  }
 
-	def posMod(a: BigInt, m: BigInt): BigInt = (a%m + m) % m
+  def posMod(a: BigInt, m: BigInt): BigInt = (a % m + m) % m
 
-	def modularInverse(a: BigInt, m: BigInt): BigInt = {
-		val (_, x, _) = gcdExtended(a, m)
-		(x%m + m) % m
-	}
+  def modularInverse(a: BigInt, m: BigInt): BigInt = {
+    val (_, x, _) = gcdExtended(a, m)
+    (x % m + m) % m
+  }
 
-	// Return gcd(a, b), x, y such that a*x + b*y = gcd(a, b)
-	def gcdExtended(a: BigInt, b: BigInt): (BigInt, BigInt, BigInt) = {
-		if (a == 0) (b, 0, 1)
-		else {
-			val (gcd, x1, y1) = gcdExtended(b%a, a)
-			val x = y1 - (b/a) * x1
-			val y = x1
+  // Return gcd(a, b), x, y such that a*x + b*y = gcd(a, b)
+  def gcdExtended(a: BigInt, b: BigInt): (BigInt, BigInt, BigInt) = {
+    if (a == 0) (b, 0, 1)
+    else {
+      val (gcd, x1, y1) = gcdExtended(b % a, a)
+      val x = y1 - (b / a) * x1
+      val y = x1
 
-			(gcd, x, y)
-		}
-	}
+      (gcd, x, y)
+    }
+  }
 
-	def gcd(a: Int, b: Int): Int = if (b == 0) a.abs else gcd(b, a % b)
-	def gcd(a: BigInt, b: BigInt): BigInt = if (b == 0) a.abs else gcd(b, a % b)
-	def lcm(list: Seq[BigInt]): BigInt = list.foldLeft(BigInt(1))((a, b) => (a / gcd(a, b)) * b)
+  def gcd(a: Int, b: Int): Int = if (b == 0) a.abs else gcd(b, a % b)
 
-	object Vector {
-		case class Pos(x: Int, y: Int) {
-			def +(direction: Dir): Pos = Pos(x + direction.x, y + direction.y)
-		}
-		case class Dir(x: Int, y: Int) {
-			def unary_- = Dir(-x, -y)
-		}
+  def gcd(a: BigInt, b: BigInt): BigInt = if (b == 0) a.abs else gcd(b, a % b)
 
-		object Dir {
-			val all8Dirs = List(
+  def lcm(list: Seq[BigInt]): BigInt = list.foldLeft(BigInt(1))((a, b) => (a / gcd(a, b)) * b)
+
+  object Vector {
+
+    case class Pos(x: Int, y: Int) {
+      def +(direction: Dir): Pos = Pos(x + direction.x, y + direction.y)
+    }
+
+    case class Dir(x: Int, y: Int) {
+      def unary_- = Dir(-x, -y)
+    }
+
+    case class PolarCoordinate(r: Double, theta: Double)
+    object PolarCoordinate {
+      def make(origo: Pos, p: Pos): PolarCoordinate =
+        PolarCoordinate(dist(origo, p), math.atan2(-(p.y - origo.y), p.x - origo.x))
+    }
+
+    def dist(p1: Pos, p2: Pos): Double = math.sqrt(math.pow(p1.x - p2.x, 2) + math.pow(p1.y - p2.y, 2))
+
+    object Dir {
+      val all8Dirs = List(
         Dir(1, 0),
         Dir(1, 1),
         Dir(0, 1),
@@ -65,9 +77,9 @@ object Util {
         Dir(-1, -1),
         Dir(0, -1),
         Dir(1, -1))
-		}
+    }
 
-	}
+  }
 
-	def normalizeNewLine(s: String) = s.replace("\r\n", "\n")
+  def normalizeNewLine(s: String) = s.replace("\r\n", "\n")
 }
